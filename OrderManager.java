790437622140a532f4d4e4f2dc6de5f7724051f6
raj1552 <anurajshrestha75@gmail.com>
2023-08-24 07:37:33 +0545

    made some changes on customer name and quantity

--- OrderManager.java
+++ OrderManager.java
@@ -1,4 +1,5 @@
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,6 +9,7 @@ public class OrderManager {
         char again;
 
         do {
+
             Order order = new Order();
             System.out.println("\n");
             String name;
@@ -16,6 +18,12 @@ public class OrderManager {
             do {
                 System.out.println("Enter Customer name ");
                 name = sc.nextLine();
+
+                // Check if the entered name contains only numeric characters
+                if (name.matches("\\d+")) {
+                    System.out.println("Please enter a valid name, not a number.");
+                    name = ""; // Reset the name so that the loop continues
+                }
             } while (name.isEmpty());
 
             boolean addMoreProducts = true;
@@ -24,16 +32,28 @@ public class OrderManager {
                 System.out.println("Enter product Id");
                 boolean productFound = false;
                 int itemid = sc.nextInt();
+                sc.nextLine();
 
-                // Iterate through the list of products to find a match for the entered item
-                // name
                 for (Product product : products) {
-                    // Check if the entered product name matches the current product in the loop
+                    // Check if the entered product id matches the current product in the loop
                     if (product.getproductid() == itemid) {
                         productFound = true;
-                        System.out.println("\nEnter quantity");
-                        int quantity = sc.nextInt();
-                        sc.nextLine();
+
+                        int quantity = -1;
+                        while (quantity <= 0) {
+                            System.out.println("\nEnter quantity");
+                            if (sc.hasNextInt()) {
+                                quantity = sc.nextInt();
+                                if (quantity <= 0) {
+                                    System.out.println("Please enter a valid quantity greater than 0.");
+                                }
+                            } else {
+                                System.out.println("Invalid input. Please enter a numeric value for quantity.");
+                                sc.next(); // Consume the invalid input
+                            }
+                        }
+
+                        sc.nextLine(); // Consume the newline character
                         order.additems(product, quantity);
                         break; // Exit the loop since product is found
                     }
@@ -45,8 +65,8 @@ public class OrderManager {
                 }
 
                 System.out.println("Do you want to add more products? (Y/N)");
-                char addMore = sc.nextLine().charAt(0);
-                addMoreProducts = (Character.toUpperCase(addMore) == 'Y');
+                String addMore = sc.nextLine().toUpperCase();
+                addMoreProducts = (addMore.charAt(0) == 'Y');
             }
 
             System.out.println("\t\t\t\t\t" + "Handicraft Happiness");
@@ -66,9 +86,10 @@ public class OrderManager {
             System.out.println("\t\t\t" + "Customer Name" + "\t\t\t\t\t" + name);
             System.out.println("\t\t\t" + "For Store" + "\t\t\t\t\t" + order.finalbill());
             System.out.println("\t\t\t" + "Charity contribution " + "\t\t\t\t" + order.forcharitycontribution());
-            System.out.println("\t\t\t" + "Total Bill" + "\t\t\t\t\t" + order.getcalulation()+"\n\n");
+            System.out.println("\t\t\t" + "Total Bill" + "\t\t\t\t\t" + order.getcalulation() + "\n\n");
             System.out.println("Do you want to repeat again? (Y/N)");
             again = sc.nextLine().charAt(0);
         } while (Character.toUpperCase(again) == 'Y');
+        System.out.println("Thankyou for visting");
     }
 }
